bba0633ce9a7d72de171f809543a49e64d8c20c1
IsMadalena <IsMadalena@user.github.com>
2021-12-28 22:08:15 +0100

    users app.

--- backend/fakestagram/settings.py
+++ backend/fakestagram/settings.py
@@ -29,6 +29,9 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+INTERNAL_APPS = [
+    'posts',
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -37,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+] + INTERNAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
